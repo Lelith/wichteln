458f500b777a68532327c86b25327c03b56869f3
lelith <lelith@gmx.de>
2016-09-12 19:59:03 +0200

    refactor images

---
+++ nav.php
@@ -0,0 +1,9 @@
+<ul class="navi">
+  <li><a href="und-so-gehts.html"> Regel </a></li>
+  <li><a href="ich-wuensche-mir.php"> Wunsch eintrage </a></li>
+  <li><a href="ich-will-was-basteln.php"> Wunsch anfrage </a></li>
+  <li><a href="lass-mich-buergen.php"> Bürge </a></li>
+  <li><a href="ich-habe-was-verschickt.php"> Versand bestätigen </a></li>
+  <li><a href="ich-habe-was-bekommen.php"> Empfang bestätigen </a></li>
+
+</ul>
