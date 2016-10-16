b362b538d790802d5e016879847c605429808ac3
lelith <lelith@gmx.de>
2016-10-16 18:55:40 +0200

    create mapping array

--- static.php
+++ static.php
@@ -12,7 +12,7 @@ $anfragen_start= 201511150000;
 $anfragen_ende=  202011290000;
 
 $senden_start=   201511150000;
-$senden_ende=    202012130000;
+$senden_ende=    201610100000;
 
 $empfangen_start=201511150000;
 $empfangen_ende= 202012300000;
@@ -21,6 +21,8 @@ $buergen_start=  201511080000;
 $buergen_ende=   202011290000;
 /* test */
 
+//status mapping
+$geschenk_status = array("noch offen", "ausgewählt", "wird bereits bewichtelt", "verschickt", "erhalten");
 
 
 /* prooduktiv
