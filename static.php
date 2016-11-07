2473b32d279a595ba91a6fc1b04f01ade1f55a08
lelith <lelith@gmx.de>
2016-11-07 21:25:28 +0100

    some css fixes

--- static.php
+++ static.php
@@ -6,19 +6,19 @@ $orgawichtel = 10714;
 $admin = 109;
 
 /* test */
-$eintragen_start=201511080000;
+$eintragen_start=201610080000;
 $eintragen_ende= 202011220000;
 
-$anfragen_start= 201511150000;
+$anfragen_start= 201610150000;
 $anfragen_ende=  202011290000;
 
-$senden_start=   201511150000;
+$senden_start=   201610150000;
 $senden_ende=    201610100000;
 
-$empfangen_start=201511150000;
+$empfangen_start=201610150000;
 $empfangen_ende= 202012300000;
 
-$buergen_start=  201511080000;
+$buergen_start=  201610080000;
 $buergen_ende=   202011290000;
 /* test */
 
