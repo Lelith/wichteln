72b15708741a26afa23210b958a255bf8578fe42
lelith <lelith@gmx.de>
2016-11-07 19:42:52 +0100

    split faq from rules

--- nav.php
+++ nav.php
@@ -5,5 +5,5 @@
   <li><a href="lass-mich-buergen.php"> Bürgen </a></li>
   <li><a href="ich-habe-was-verschickt.php"> Versand bestätigen </a></li>
   <li><a href="ich-habe-was-bekommen.php"> Empfang bestätigen </a></li>
-
+  <li><a href="faq.php"> FAQ </a></li>
 </ul>
